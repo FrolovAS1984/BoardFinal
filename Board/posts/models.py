e6a7a0f70572b6a0024d68b1507ec1f12eec325c
Frolov_AS <slaider84@list.ru>
2023-06-02 15:46:12 +0300

    Initial commit

---
+++ Board/posts/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Article(models.Model):
+    TYPE = (
+        ('tank', 'Танки'),
+        ('heal', 'Хилы'),
+        ('dd', 'ДД'),
+        ('buyers', 'Торговцы'),
+        ('gildemaster', 'Гилдмастеры'),
+        ('quest', 'Квестгиверы'),
+        ('smith', 'Кузнецы'),
+        ('tanner', 'Кожевники'),
+        ('potion', 'Зельевары'),
+        ('spellmaster', 'Мастера заклинаний'),
+    )
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    text = models.TextField()
+    category = models.CharField(max_length=8, choices=TYPE, default=None)
+    upload = models.FileField(upload_to='uploads/')
+
+
+class UserResponse(models.Model):
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    article = models.ForeignKey(Article,on_delete=models.CASCADE)
+    status = models.BooleanField(default=False)
